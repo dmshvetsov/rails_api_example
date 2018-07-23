beb206dfc669f7855e9fa6e8725dc4fdd31c952c
shvetsovdm <sorrows@mail.ru>
2018-07-23 16:51:18 +1000

    Move PublisherShopsResponse into controller class
    because it is intended for use only in PublishersShopsController

--- app/controllers/v1/publishers_shops_controller.rb
+++ app/controllers/v1/publishers_shops_controller.rb
@@ -11,7 +11,7 @@ class V1::PublishersShopsController < ApplicationController
     books_ids, shops_ids = BooksInStock.where(publisher_id: publisher_id).pluck(:id, :shop_id).transpose
     return [] unless books_ids || shops_ids
 
-    PublisherShopsResponse.new(
+    Response.new(
       shops: PublisherShop.most_sold(id: shops_ids, publisher_id: publisher_id),
       books_in_stock: BooksInStock.where(id: books_ids).group_by(&:shop_id)
     )
@@ -20,29 +20,30 @@ class V1::PublishersShopsController < ApplicationController
   def publisher_id
     params[:publisher_id]
   end
-end
 
-class PublisherShopsResponse
-  BOOKS_ATTRS = %i[id title copies_in_stock].freeze
+  # Response for publisher shops API
+  class Response
+    BOOKS_ATTRS = %i[id title copies_in_stock].freeze
 
-  def initialize(shops: [], books_in_stock: {})
-    @shops = shops
-    @books_in_stock = books_in_stock
-  end
-
-  def as_json(options = nil)
-    shops.map do |shop|
-      shop_books = books_in_stock[shop.id].as_json(only: BOOKS_ATTRS)
-      {
-        id: shop.id,
-        name: shop.name,
-        books_sold_count: shop.books_sold_count,
-        books_in_stock: shop_books
-      }
+    def initialize(shops: [], books_in_stock: {})
+      @shops = shops
+      @books_in_stock = books_in_stock
     end
+
+    def as_json(options = nil)
+      shops.map do |shop|
+        shop_books = books_in_stock[shop.id].as_json(only: BOOKS_ATTRS)
+        {
+          id: shop.id,
+          name: shop.name,
+          books_sold_count: shop.books_sold_count,
+          books_in_stock: shop_books
+        }
+      end
+    end
+
+    private
+
+    attr_reader :shops, :books_sold, :books_in_stock
   end
-
-  private
-
-  attr_reader :shops, :books_sold, :books_in_stock
 end
