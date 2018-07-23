069b52e33924a4a9bd3bd6fcd8a83f1ac3b6811d
shvetsovdm <sorrows@mail.ru>
2018-07-23 12:03:45 +1000

    Add seeds data for dev.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+exit unless Rails.env.development?
+
+# Truncate data
+
+[
+  Shop::StockChange,
+  Shop,
+  Book,
+  Publisher
+].map(&:delete_all)
+
+# Create data
+
+publisher1 = Publisher.create!(id: 1)
+publisher2 = Publisher.create!(id: 2)
+Publisher.create!(id: 3)
+
+book1, book2, book3, book4 = Book.create!([
+  { publisher: publisher1, title: 'SQL cookbook' },
+  { publisher: publisher1, title: 'Code: The Hidden Language of Computer Hardware and Software' },
+  { publisher: publisher1, title: 'HTML and CSS: Design and Build Websites' },
+  { publisher: publisher2, title: 'Clean code' }
+])
+
+amazon = Shop.create!(name: 'Amazon')
+ozon = Shop.create!(name: 'Ozon')
+
+Shop::StockChange.create!([
+  { shop: amazon, book: book1, quantity: 2105 },
+  { shop: amazon, book: book2, quantity: 1350 },
+  { shop: amazon, book: book3, quantity: 4200 },
+  { shop: amazon, book: book4, quantity: 800 },
+  { shop: ozon, book: book4, quantity: 400 }
+])
